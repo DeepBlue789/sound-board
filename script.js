5a1e2462470fced5028734909c731adfb9d0442c
Nolwenn Le Pelleter <le_pelleter_nolwenn@yahoo.fr>
2020-09-21 14:34:21 +0200

    Ajout des sons

---
+++ script.js
@@ -0,0 +1,30 @@
+const sounds = [
+    'dog',
+    'bird',
+    'cow'
+]
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+
+    btn.classList.add('btn')
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () =>{
+        stopSound();
+        document.getElementById(sound).play();
+    })
+
+    document.body.appendChild(btn);
+
+});
+
+
+function stopSound() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause()
+        song.currentTime = 0;
+    })
+}
